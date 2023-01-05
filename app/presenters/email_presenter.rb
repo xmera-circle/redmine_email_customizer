13d04945d84799d6cb928019184e4651dff4e53b
Liane Hampe <liaham@xmera.de>
2023-01-05 15:22:47 +0100

    Updates copyright year

--- app/presenters/email_presenter.rb
+++ app/presenters/email_presenter.rb
@@ -2,7 +2,7 @@
 
 # This file is part of the Plugin Redmine Custom Email Styling.
 #
-# Copyright (C) 2022 Liane Hampe <liaham@xmera.de>, xmera.
+# Copyright (C) 2022-2023 Liane Hampe <liaham@xmera.de>, xmera Solutions GmbH.
 #
 # This plugin program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License
